7ec87fe61153c65711ebead5dea6ac551b7eb693
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2026-09-23 09:07:57 -0300

    chore: Atualização link

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // Import de bibliotecas
 import './App.css';
-import { BrowserRouter, Routes, Route, Outlet, useNavigate, useParams } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet, useNavigate, useParams, Link } from "react-router-dom";
 import { useState , useEffect } from 'react';
 import { Container, Form, Button, Table, Nav } from 'react-bootstrap';
 
@@ -21,11 +21,11 @@ function Layout(){
         <h1>Menu principal</h1>
         <Nav defaultActiveKey="/" className="bg-light flex-column">
           <Nav.Item as="incluir">
-            <Nav.Link href="/frmcadastrocliente/-1">1. Incluir</Nav.Link>
+              <Nav.Link as={Link} to="/frmcadastrocliente/-1">1. Incluir</Nav.Link>
           </Nav.Item>
           <Nav.Item as="listar">
-            <Nav.Link href="/frmlistarcliente">2. Listar(Alterar, Excluir)</Nav.Link>
-          </Nav.Item>     
+              <Nav.Link as={Link} to="/frmlistarcliente">2. Listar(Alterar, Excluir)</Nav.Link>
+          </Nav.Item>      
         </Nav>
         <hr />
         <Outlet />
